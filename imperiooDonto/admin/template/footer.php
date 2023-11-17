6528cb33fb0a378cedf9c7a319ab7e6a3095e322
Paulo Cesar <paulosenaisesiorg@gmail.com>
2023-11-17 11:52:08 -0300

    Others pages

--- imperiooDonto/admin/template/footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-</div>
-<footer>
-    <script src="/js/bootstrap.bundle.min.js"></script>
-    <script src="/js/dashboard.js"></script>
-    <script src="/js/sidebars.js"></script>
-</footer>
-</body>
-</html>
--- imperiooDonto/admin/template/main-home.php
+++
@@ -1,8 +0,0 @@
-<main class = "col-md-9 ms-sm-auto col-lg-10 px-md-4">
-    <div class = "d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2">
-        <h1 class= "h2">Home</h1>
-    </div>
-    
-
-</div>
-</main>
